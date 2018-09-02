73ac24711ed03daec77576b8544683b393a2b2b0
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2018-09-02 21:31:25 +0100

    Added planet graph calc source and post deploy script to build the binary and set the properties also added a cron job to refresh the MPC and comet data

---
+++ markmcintyreastro/planetgraphs/src/OrbitCalcs.cpp
@@ -0,0 +1,401 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <math.h>
+#include <stdio.h>
+#include "OrbitCalcs.h"
+
+// model taken from http://www.stjarnhimlen.se/comp/ppcomp.html
+
+char szPath[512]; // path to the DLL and asteroid and comet data files
+char szOutputPath[512]; // path to the exported datafiles
+char szErrorPath[512]; // path to the log file
+int maxloaded = 0; // number of objects loaded, including the planets sun and moon
+
+struct OrbitalElements *elements=NULL;
+
+double __stdcall GetOrbitalParam(int planetno, int what)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+
+	planetno = (planetno > maxloaded ? maxloaded : planetno);
+
+	if (what == 'N' || what =='n')
+		return LongOfAscNode(planetno, 0);
+	if (what == 'i' || what =='I')
+		return Inclination(planetno, 0);
+	if (what == 'w' || what =='W')
+		return ArgOfPerihelion(planetno, 0);
+	if (what == 'a' || what == 'A')
+		return elements[planetno].a[0] *(planetno == MOON ? ERAD : AU);
+	if (what == 'e' || what=='E')
+		return Eccentricity(planetno,0);
+	if (what == 'M' || what =='m')
+		return MeanAnomaly(planetno, 0);
+	if (what == 'G' || what =='G')
+		return elements[planetno].mag[0];
+	if (what == 's' || what == 'S')
+		return elements[planetno].siz *(planetno == MOON ? ERAD*60 : AU) / (3600.0 * RAD2DEG);
+	if (what == 'T' ||  what =='t')
+		return (double)(elements[planetno].epoch[0]*10000+ elements[planetno].epoch[1] * 10+ elements[planetno].epoch[2]);
+	return 0.0;
+}
+
+double __stdcall MeanAnomaly(int planetno , double dd ) 
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+
+	double k = elements[planetno].MA[0];
+	double p = elements[planetno].MA[1];
+
+	double ma = (k + p * dd) / RAD2DEG;
+	while (ma > TWOPI)
+		ma = ma - TWOPI;
+	while (ma < 0)
+		ma = ma + TWOPI;
+	return ma ;
+}
+
+double __stdcall LongOfAscNode(int planetno, double d)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+
+	double n1 = elements[planetno].N[0];
+	double n2 = elements[planetno].N[1];
+	double n = (n1 + n2 * d);
+	while (n > TWOPI)
+		n -= TWOPI;
+	while (n < 0)
+		n += TWOPI;
+	return n;
+}
+
+double __stdcall Eccentricity(int planetno, double d)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+	double e1 = elements[planetno].e[0];
+	double e2 = elements[planetno].e[1];
+	return (e1 + e2 * d);
+}
+
+double __stdcall Inclination(int planetno, double d)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+	double e1 = elements[planetno].incl[0];
+	double e2 = elements[planetno].incl[1];
+	return (e1 + e2 * d)/RAD2DEG;
+}
+
+double __stdcall EccentricAnomaly(double m, double e)
+{
+	double deltae = 1;
+	double e0 = m + e * sin(m) * (1.0 + e * cos(m));
+	if (e0==0) return 0;
+
+	while (deltae > 0.00000001)
+	{
+		double e1 = e0 - (e0 - e * sin(e0) - m) / (1 - e * cos(e0));
+		deltae = fabs(e1 - e0);
+		e0 = e1;
+	}
+	return e0;
+}
+
+double __stdcall ArgOfPerihelion(int planetno, double dd)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+	double k = elements[planetno].omega[0];
+	double d = elements[planetno].omega[1];
+	double aop = (k + d * dd) / RAD2DEG;
+	while (aop > TWOPI)
+		aop = aop - TWOPI;
+	while (aop < 0)
+		aop = aop + TWOPI;
+	return aop;
+}
+double __stdcall ObliquityofEcliptic(double dd)
+{
+	return (23.4392911 - 0.000000356115 * dd)/RAD2DEG;
+}
+
+double __stdcall PrecessionCorr(double epoch, double dd)
+{
+	return (0.0000382394 * (365.2422 * (epoch - 2000.0) - dd));
+}
+
+double __stdcall SunLongitude(double v, double dd)
+{
+	if (!elements) return 0;
+	double w = ArgOfPerihelion(SUN, dd);
+	return (v + w);
+}
+
+double __stdcall SunRA(double dd)
+{
+	if (!elements) return 0;
+	return (PlanetXYZ(SUN, dd, 6, 0, 0, 0, 0));
+}
+double __stdcall SunDec(double dd)
+{
+	if (!elements) return 0;
+	return (PlanetXYZ(SUN, dd, 7, 0, 0, 0, 0));
+}
+
+
+double __stdcall PlanDist(int planetno, double dd)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+	if (planetno == PLUTO)
+	{
+		double lonecl, latecl, r;
+		DoPluto(dd, lonecl, latecl, r);
+		return r;
+	}
+	else
+	{
+		double a = elements[planetno].a[0] + dd * elements[planetno].a[1];
+		double m = MeanAnomaly(planetno, dd);
+		double e = Eccentricity(planetno, dd);
+		double ea = EccentricAnomaly(m, e);
+		double xv = cos(ea) - e;
+		double yv = sqrt(1.0 - e*e) * sin(ea);
+		return (a * sqrt(xv*xv + yv*yv));
+	}
+}
+
+double __stdcall PlanTrueAnomaly(int planetno, double d)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+	double m = MeanAnomaly(planetno, d);
+	double e = Eccentricity(planetno, d);
+	double ea = EccentricAnomaly(m, e);
+	double xv = (cos(ea) - e);
+	double yv = (sqrt(1.0 - e*e) * sin(ea));
+	double pta = atan2(yv, xv);
+
+	while (pta > TWOPI)
+		pta = pta - TWOPI;
+	while (pta < 0)
+		pta = pta + TWOPI;
+	return pta;
+}
+
+double __stdcall AzFromRADec(double  lst, double ra, double dec, double lat, int zora, double temp, double pres)
+{
+	double ha = (lst * 360 - ra) / RAD2DEG;
+	dec = dec / RAD2DEG;
+	lat = lat / RAD2DEG;
+
+	double x = cos(ha) * cos(dec);
+	double y = sin(ha) * cos(dec);
+	double Z = sin(dec);
+
+	double xhor = x * sin(lat) - Z * cos(lat);
+	double yhor = y;
+	double zhor = x * cos(lat) + Z * sin(lat);
+
+	double az = atan2(yhor, xhor) * RAD2DEG + 180.0;
+	if (zora == 0)
+		return az;
+	else
+	{
+		double alti = atan2(zhor, sqrt(xhor*xhor + yhor*yhor));
+		// correction for atmospheric distortion - after Saemundsson
+		double h = alti*RAD2DEG;
+		double hh = h + (10.3 / (h + 5.11));
+		double r = 1.02 / tan(hh/RAD2DEG);
+
+		//temperature & pressure correction - above formula assumes P=1010 and T=10
+		r = r * pres / 1010 * 283.0 / (temp + 273.0);
+
+		return (alti * RAD2DEG + r / 60.0);
+	}
+}
+
+double __stdcall AltAtTransit(int planetno, double dtval, double lat, double longi, double temp, double pres)
+{
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+	double tt = TimeofTransit(planetno, dtval, lat, longi);
+	double dd = AstroDaysFromDt(dtval + tt / 24.0);
+	double lst = LSTFromDt(dtval + tt/24.0, longi)/24.0;
+	return PlanetXYZ(planetno, dd, 8, lst, lat, temp, pres);
+}
+
+double __stdcall PlanetXYZ(int planetno, double dd, int xyz, double lst, double lat, double temp, double pres)
+{
+	double lonecl, latecl, r, v;
+	double xh, yh, zh, ra, decl, azi, alti;
+
+	if (!elements || planetno >= NUMELEMENTS) return 0;
+
+	if (planetno != PLUTO)
+	{
+		double w = ArgOfPerihelion(planetno, dd);
+		double i = Inclination(planetno, dd);
+		double m = MeanAnomaly(planetno, dd);
+		double e = Eccentricity(planetno, dd);
+		double N = (elements[planetno].N[0] + dd * elements[planetno].N[1]) / RAD2DEG;
+
+		if (e < KEPLERIANLIMIT) // eccentricity isn't too crazy
+		{
+			double a = elements[planetno].a[0] + dd * elements[planetno].a[1];
+			double ea = EccentricAnomaly(m, e);
+
+			// location in its own orbit
+			double xv = cos(ea) - e;
+			double yv = sqrt(1.0 - e *e) * sin(ea);
+
+			// distance and true anomaly from perihelion
+			r = a * sqrt(xv*xv + yv*yv);
+			v = atan2(yv, xv);
+		}
+		else // comet with high eccentricity and possibly even parabolic
+		{
+			double q = elements[planetno].a[0]; // perihelion distance
+			double dc = elements[planetno].MA[0]; // datetime of perihelion
+			double deltad = dd - dc;
+
+			if (fabs(e-1) < 1e-7) //parabolic
+			{
+				double H = deltad * (KEPLERIANCONST / sqrt(2)) / pow(q, 1.5);
+				double h = 1.5 * H;
+				double g = sqrt(1.0 + h*h);
+				double s = pow(g + h, 1.0 / 3.0) - pow(g - h, 1.0 / 3.0);
+
+				// distance and true anomaly from perihelion
+				v = 2.0 * atan(s);
+				r = q* (1.0 + s*s); 
+			}
+			else // near parabolic - accurate up to about 40 AU
+			{
+				double a = 0.75 * deltad * KEPLERIANCONST * sqrt((1 + e) / (q*q*q));
+				double b = sqrt(1 + a*a);
+				double W = pow(b + a, 1.0/3.0) - pow(b - a,1.0/3.0);
+				double f = (1 - e) / (1 + e);
+
+				double a1 = (2.0 / 3.0) + (2.0 / 5.0) * W*W;
+				double a2 = (7.0 / 5.0) + (33.0 / 35.0) * W*W + (37.0 / 175.0) * W*W*W*W;
+				double a3 = W*W * ((432.0 / 175.0) + (956.0 / 1125.0) * W*W + (84.0 / 1575.0) * W*W*W*W);
+
+				double C = W*W / (1 + W*W);
+				double g = f * C*C;
+				double w = W * (1 + f * C * (a1 + a2*g + a3*g*g));
+
+				// distance and true anomaly from perihelion
+				v = 2 * atan(w);
+				r = q * (1 + w*w) / (1 + w*w * f);
+			}
+		}
+
+		// calculate heliocentric coords excluding perturbations
+		// nb these coords are *geo*centric for the moon
+		xh = r * (cos(N) * cos(v + w) - sin(N) * sin(v + w) * cos(i));
+		yh = r * (sin(N) * cos(v + w) + cos(N) * sin(v + w) * cos(i));
+		zh = r * (sin(v + w) * sin(i));
+
+		r = sqrt(xh*xh + yh*yh + zh*zh);
+
+		// coords in ecliptic plane
+		lonecl = atan2(yh, xh);
+		latecl = atan2(zh, sqrt(xh*xh + yh*yh));
+	}
+	else
+	{
+		// crazy speshul calcs for pluto - nonanalytic, only accurate till 2100
+		DoPluto(dd, lonecl, latecl, r);
+	}
+
+	if (planetno == MOON)
+	{
+		//moon perturbations
+		double errlon = MoonPerturbations(dd, 1);
+		double errlat = MoonPerturbations(dd, 2);
+		double errd = MoonPerturbations(dd, 3);
+		lonecl = lonecl + errlon;
+		latecl = latecl + errlat;
+		r = r + errd;
+	}
+	else if (planetno > URANUS)
+	{
+		// nothing needed for the very outer planets
+	}
+	else if (planetno > MARS && planetno < PLUTO)
+	{
+		// jupiter, saturn and uranus
+		// longitude adjustment for all three
+		double errlon = GasPerturbations(planetno, dd, 1);
+		// latutide correction too for saturn
+		double errlat = 0;
+		if (planetno == SATURN)
+			errlat = GasPerturbations(planetno, dd, 2);
+		lonecl = lonecl + errlon;
+		latecl = latecl + errlat;
+	}
+	if (lonecl < 0)
+		lonecl = lonecl + TWOPI;
+	// convert perturbed ecliptic coords back to heliocentric
+	xh = r * cos(lonecl) * cos(latecl);
+	yh = r * sin(lonecl) * cos(latecl);
+	zh = r * sin(latecl);
+
+	// convert to geocentric coordinates - not needed for moon
+	if (planetno > MOON)
+	{
+		//calculate position of sun
+		v = PlanTrueAnomaly(SUN, dd);
+		r = PlanDist(SUN, dd);
+		double ls = SunLongitude(v, dd);
+		double xs = r * cos(ls);
+		double ys = r * sin(ls);
+
+		//add to x-y coords of planet; sun is on ecliptic so z adjust is zero
+		xh = xh + xs;
+		yh = yh + ys;
+	}
+
+	// work out the distance from the earth
+	double distearth = sqrt(xh*xh + yh*yh + zh*zh);
+	if (xyz > 5 && xyz < 10)
+	{
+		//calculate RA and DEC
+		double ecl = ObliquityofEcliptic(dd);
+		double xe = xh;
+		double ye = yh * cos(ecl) - zh * sin(ecl);
+		double ze = yh * sin(ecl) + zh * cos(ecl);
+		ra = atan2(ye, xe) * RAD2DEG;
+		decl = atan2(ze, sqrt(xe*xe + ye*ye)) * RAD2DEG;
+		while (ra < 0)
+			ra = ra + 360.0;
+		while (ra > 360.0)
+			ra = ra - 360.0;
+
+		azi = AzFromRADec(lst, ra, decl, lat, 0, temp, pres);
+		alti = AzFromRADec(lst, ra, decl, lat, 1, temp, pres);
+
+		// correct to topographic coords (surface of earth)
+		// mainly relevant for the moon but worth doing for the others
+		alti = alti * PI / 180;
+		if (planetno == MOON)
+			alti = alti - cos(alti) * asin(1 / r);
+		else
+			alti = alti - (8.794 / 3600.0) / RAD2DEG / r;
+
+		alti = alti * RAD2DEG;
+	}
+	// results
+	switch (xyz)
+	{
+	case 1:		return xh;
+	case 2:		return yh;
+	case 3:		return zh;
+	case 4: 	return lonecl * RAD2DEG;
+	case 5:		return latecl * RAD2DEG;
+	case 6:		return ra;
+	case 7:		return decl;
+	case 8:		return alti;
+	case 9:		return azi;
+	case 10:	return distearth;
+	default:	return 0;
+	}
+}
+
+
---
+++ markmcintyreastro/planetgraphs/src/Outputter.cpp
@@ -0,0 +1,181 @@
+#include "OrbitCalcs.h"
+
+// shaddap with the fopen warnings
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+
+double MyRound(double input, int places, int updown);
+void addHeader(FILE* f, char* pname, char* typ);
+void addFooter(FILE* f, char* unt, char* typ, double minv, double maxv);
+
+void addHeader(FILE* f, char* pname, char* typ)
+{
+	fprintf(f, "$(function() {\n");
+	fprintf(f, "Morris.Line({\n");
+	fprintf(f, "  element: 'planet-%s',\n", typ);
+	fprintf(f, "data: [ \n");
+}
+
+void addFooter(FILE* f, char* unt, char* typ, double minv, double maxv)
+{
+	fprintf(f, "        xkey: 'time',\n");
+	fprintf(f, "        ykeys: ['%s'],\n", typ);
+	fprintf(f, "        labels: ['%s'],\n", typ);
+	fprintf(f, "        hideHover: 'auto',\n");
+	fprintf(f, "        xLabelAngle: 45,\n");
+	fprintf(f, "        ymax: %.0lf ,\n", maxv);
+	fprintf(f, "        ymin: %.0lf,\n", minv);
+	fprintf(f, "        postUnits: '%s',\n", unt);
+	fprintf(f, "        resize: true\n");
+	fprintf(f, "    });\n");
+	fprintf(f, "});\n");
+}
+
+
+void SaveOrbitalElements(void)
+{
+	char fileloc[256];
+	sprintf(fileloc, "%s/orbitalelements.csv", szPath);
+	FILE* f = fopen(fileloc, "w");
+	for (int i = 0; i < 10; i++)
+	{
+		fprintf(f, "%s,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+			elements[i].name, 
+			elements[i].N[0], elements[i].N[1],
+			elements[i].incl[0], elements[i].incl[1],
+			elements[i].omega[0], elements[i].omega[1],
+			elements[i].a[0], elements[i].a[1],
+			elements[i].e[0], elements[i].e[1],
+			elements[i].MA[0], elements[i].MA[1],
+			elements[i].mag[0], elements[i].mag[1],
+			elements[i].siz);
+	}
+	fclose(f);
+}
+
+void CreateOutputFiles(double lati, double longi, double dt)
+{
+	int planetno;
+	char fnam[256];
+	FILE *f5;
+
+	sprintf(fnam, "%s/ObjNames.js", szOutputPath);
+	f5 = fopen(fnam, "w");
+
+	for(planetno = MERCURY; planetno <= maxloaded; planetno++)
+	{
+		if (planetno == EARTH) planetno++;
+		FILE *f1, *f2, *f3, *f4;
+		char* pname = elements[planetno].name;
+
+		fprintf(f5, "%s\n", pname);
+
+		if (pname[0] == '(')
+		{
+			char tmpnam[256] = { 0 };
+			int j = 0;
+			for (int i = 1; i < (int)strlen(pname); i++)
+			{
+				if (pname[i] == ')') pname[i] = '-';
+				if (pname[i] == ' ') i++;
+				tmpnam[j++] = pname[i];
+			}
+			strcpy(pname, tmpnam);
+		}
+
+		int intvl = (planetno < MARS|| planetno > PLUTO ? 7 : 30);
+		int maxiters = (planetno < MARS || planetno > PLUTO ? 104 : 72);
+		if (planetno > LASTASTEROID)
+		{ // comets - daily for 3 months
+			intvl = 1;
+			maxiters = 91;
+		}
+
+		sprintf(fnam, "%s/%sAltitude.js", szOutputPath, pname);
+		f1 = fopen(fnam, "w");
+		if(!f1) 
+		{ 
+			char errfnam[256];
+			sprintf(errfnam, "%s/orbitcalcs.err", szErrorPath);
+			FILE *errf = fopen(errfnam,"w+");
+			fprintf(errf, "unable to open %s for writing\n", fnam);
+			fclose(errf);
+			return;
+		}
+		sprintf(fnam, "%s/%sMagnitude.js", szOutputPath, pname);
+		f2 = fopen(fnam, "w");
+		sprintf(fnam, "%s/%sSize.js", szOutputPath, pname);
+		f3 = fopen(fnam, "w");
+		sprintf(fnam, "%s/%sData.csv", szOutputPath, pname);
+		f4 = fopen(fnam, "w");
+
+		addHeader(f1, pname, "altitude");
+		addHeader(f2, pname, "magnitude");
+		addHeader(f3, pname, "size");
+		double minsiz = 100;
+		double maxsiz = 0;
+		double maxbri = -20;
+		double minbri = 200;
+		double minalt = 0;
+		double maxalt = 0;
+		fprintf(f4, "Date,Altitude,Magnitude,size\n");
+
+		for (int i = 0; i <= maxiters; i++)
+		{
+			double alti = IsVisible(planetno, dt + intvl * i, lati, longi, 1, 1, 10, 1010);
+			double best = IsVisible(planetno, dt + intvl * i, lati, longi, 1, 2, 10, 1010);
+			double brig = VisualMagnitude(planetno, AstroDaysFromDt(dt + intvl * i));
+			double siz = ApparentSize(planetno, AstroDaysFromDt(dt + intvl * i));
+
+			if (siz > maxsiz) maxsiz = siz;
+			if (siz < minsiz) minsiz = siz;
+			if (brig > maxbri) maxbri = brig;
+			if (brig < minbri) minbri = brig;
+			if (alti > maxalt) maxalt = alti;
+//			long tmst = DtvalToUnixTS(dt + intvl * i);
+			long tms2 = DtvalToUnixTS(dt + intvl * i + best);
+
+			if (i < maxiters)
+			{
+				fprintf(f1, "{time: %ld000, altitude: %.2lf},\n", tms2, alti);
+				fprintf(f2, "{time: %ld000, magnitude: %.2lf},\n", tms2, brig);
+				fprintf(f3, "{time: %ld000, size: %.2lf},\n", tms2, siz);
+			}
+			else
+			{
+				fprintf(f1, "{time: %ld000, altitude: %.2lf}],\n", tms2, alti);
+				fprintf(f2, "{time: %ld000, magnitude: %.2lf}],\n", tms2, brig);
+				fprintf(f3, "{time: %ld000, size: %.2lf}],\n", tms2, siz);
+			}
+			int yy, mo, dy, hh, mm, ss;
+			GetDateFromDtval(dt + intvl * i + best, yy, mo, dy, hh, mm, ss);
+			
+			fprintf(f4, "%4.4d-%2.2d-%2.2d %2.2d:%2.2d:%2.2d, %.2lf, %.2lf, %.2lf\n", yy, mo, dy, hh, mm, ss, alti, brig, siz);
+		}
+
+		maxbri = MyRound(maxbri, 0, 1);
+		minbri = MyRound(minbri, 0, 2);
+		maxsiz = MyRound(maxsiz * 1.2, 2, 1);
+		minsiz = MyRound(minsiz * 0.8, 2, 2);
+		maxalt = MyRound(maxalt * 1.2, 2, 1);
+
+		addFooter(f1, "\\xB0", "altitude", minalt, maxalt);
+		fclose(f1);
+		addFooter(f2, " mag", "magnitude", maxbri, minbri);
+		fclose(f2);
+		addFooter(f3, " as", "size", minsiz, maxsiz);
+		fclose(f3);
+		fclose(f4);
+	}
+	fclose(f5);
+}
+
+double MyRound(double input, int places, int updown)
+{
+	double multi = pow(10, places);
+	return updown == 1 ? ceil(input*multi) / multi: floor(input*multi) / multi;
+}
+
