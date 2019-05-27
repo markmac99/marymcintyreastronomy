63e31b5fdcf064a488410423399238655b1a01f1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2019-05-27 21:12:02 +0100

    Fixed bug in conjunction calcs

--- markmcintyreastro/planetgraphs/pgpreprocess.sh
+++ markmcintyreastro/planetgraphs/pgpreprocess.sh
@@ -3,4 +3,5 @@ cd /var/www/html/markmcintyreastro/planetgraphs
 ./cometpp ./CometEls.txt ./VisCometEls.txt /var/www/html/markmcintyreastro/planetgraphs
 ./pgsummary ./CometEls.txt ./data/planetpositions.js /var/www/html/markmcintyreastro/planetgraphs
 cd data
-./conjunctioncalcs 51.88 -1.31 2
+../conjunctioncalcs 51.88 -1.31 2 2
+
