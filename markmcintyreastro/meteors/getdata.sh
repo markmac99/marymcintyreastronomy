f552935a688f3f6a118c723843e39ca9dc8f23d1
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2019-10-09 11:59:51 +0100

    Added radio section to site

--- markmcintyreastro/meteors/getdata.sh
+++ markmcintyreastro/meteors/getdata.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+cd `dirname $0`
 aws s3 cp s3://mjmm-meteor-uploads . --recursive
 mv -f meteorlist.html meteorlist.html.old
+mv -f radiolist.html radiolist.html.old
+mkdir radio
 ls -1r *.csv | while read i; do echo \<a href=\"$i\"\>$i\</a\>\<br\> >> meteorlist.html ; done
+echo \<b\>Live screenshot - last refreshed `date '+%Y-%m-%d %H:%M:%S'` UT\</b\>\<br\> >> radiolist.html
+echo \<a href=\"radio/latest2d.jpg\"\>\<img src=\"radio/latest2d.jpg\"\ width=470\>\</a\>\<br\> >> radiolist.html
+echo \<b\>Latest radio meteor capture\</b\>\<br\> >> radiolist.html
+echo \<a href=\"radio/latestcapture.jpg\"\>\<img src=\"radio/latestcapture.jpg\"\ width=470\>\</a\>\<br\> >> radiolist.html
