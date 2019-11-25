b2f5da445b3830bbfc3060acf97660719665f31d
Mark McIntyre <mark.jm.mcintyre@cesmail.net>
2019-11-25 22:20:00 +0000

    Addeed more handling for meteor data

--- markmcintyreastro/meteors/getdata.sh
+++ markmcintyreastro/meteors/getdata.sh
@@ -5,6 +5,8 @@ mv -f meteorlist.html meteorlist.html.old
 mv -f radiolist.html radiolist.html.old
 mkdir radio
 ls -1r *.csv | while read i; do echo \<a href=\"$i\"\>$i\</a\>\<br\> >> meteorlist.html ; done
+echo \<b\>This Months Heatmap - last refreshed `date '+%Y-%m-%d %H:%M:%S'` UT\</b\>\<br\> >> radiolist.html
+echo \<a href=\"radio/RMOB_latest.jpg\"\>\<img src=\"radio/RMOB_latest.jpg\"\ width=470\>\</a\>\<br\> >> radiolist.html
 echo \<b\>Live screenshot - last refreshed `date '+%Y-%m-%d %H:%M:%S'` UT\</b\>\<br\> >> radiolist.html
 echo \<a href=\"radio/latest2d.jpg\"\>\<img src=\"radio/latest2d.jpg\"\ width=470\>\</a\>\<br\> >> radiolist.html
 echo \<b\>Latest radio meteor capture\</b\>\<br\> >> radiolist.html
